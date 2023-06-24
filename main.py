81c068bb762566a8cdcfdaff419c24e07bf0853a
Ryuk264 <73334626+Ryuk264@users.noreply.github.com>
2023-06-24 15:03:09 +0530

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import os
+import pandas as pd
+
+from deepface import DeepFace
+
+
+data = {
+    "Name":[],
+    "Age":[],
+    "Gender":[],
+    "Race":[]
+}
+
+
+for file in os.listdir("images"):
+    result = DeepFace.analyze(cv2.imread(f"images/{file}"),actions=("age","gender","race"))
+    data["Name"].append(file.split(".")[0])
+    data["Age"].append(result[0]["age"])
+    data["Gender"].append(result[0]["dominant_gender"])
+    data["Race"].append(result[0]["dominant_race"])
+    
+df = pd.DataFrame(data)
+print(df)
+
+df.to_csv("people.csv")
